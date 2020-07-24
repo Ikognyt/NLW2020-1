f8b73c579fc4725003896a47929e62ebcbfcf24b
IKOGNYT <lipe327@gmail.com>
2020-07-24 11:45:20 -0300

    Application complete. Mobile, web and server finished, enjoy !

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -19,8 +19,16 @@ class PointsController {
         .distinct()
         .select('points.*');
 
-        return response.json(points);
+        const serializedPoints = points.map(point => {
+            return {
+                ...point,           
+                image_url: `http://192.168.1.15:3333/uploads/${point.image}`,
+            };
+        })
+
+        return response.json(serializedPoints);
     }
+
 
     async show(request: Request, response: Response){
         // Request of id
@@ -34,6 +42,11 @@ class PointsController {
             return response.status(400).json({ message: 'Point not found.'});
         }
 
+        const serializedPoint =  {
+                ...point,           
+                image_url: `http://192.168.1.15:3333/uploads/${point.image}`,
+        }
+
 
         // Query all items that are related to point
         const items = await knex('items')
@@ -42,7 +55,7 @@ class PointsController {
             .select('items.title');
 
         // If find out the point, return is ok.
-        return response.json({ point, items});
+        return response.json({ point: serializedPoint, items});
     }
 
     async create(request: Request, response: Response) {
@@ -60,7 +73,7 @@ class PointsController {
         const trx = await knex.transaction();
         
         const point = {
-            image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+            image: request.file.filename,
             name,
             email,
             whatsapp,
@@ -74,10 +87,13 @@ class PointsController {
         
         const point_id = insertedIds[0];
     
-        const pointItems = items.map((item_id: number) => {
+        const pointItems = items
+        .split(',')
+        .map((item: string) => Number(item.trim()))
+        .map((item_id: number) => {
             return {
                 item_id,
-                point_id
+                point_id,
             };
         });
     
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 import { LeafletMouseEvent } from 'leaflet';
 import api from '../../services/api';
 
+import Dropzone  from '../../components/Dropzone';
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -27,7 +28,7 @@ interface IBGEUFResponse {
 
 interface IBGECityResponse {
   nome:string;
-}
+};
 
 const CreatePoint = () => {
   const [items, setItems ] = useState<Item[]>([]);
@@ -45,6 +46,8 @@ const CreatePoint = () => {
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+  const [selectedFile, setSelectedFile] = useState<File>();
+
 
   const history = useHistory();
 
@@ -132,16 +135,23 @@ useEffect(() => {
   const [latitude, longitude] = selectedPosition;
   const items = selectedItems;
 
-  const data = {
-    name,
-    email,
-    whatsapp,
-    uf,
-    city,
-    latitude,
-    longitude,
-    items
-  };
+  const data = new FormData();
+
+
+  data.append('name', name);
+  data.append('email', email);
+  data.append('whatsapp', whatsapp);
+  data.append('uf', uf);
+  data.append('city', city);
+  data.append('latitude', String(latitude));
+  data.append('longitude', String(longitude));
+  data.append('items', items.join(','));
+  
+
+  if(selectedFile){
+    data.append('image', selectedFile);
+  }
+
 
   await api.post('/points', data);
 
@@ -160,6 +170,8 @@ useEffect(() => {
       </header>
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br /> ponto de coleta</h1>
+
+        <Dropzone onFileUploaded={setSelectedFile} />
 
         <fieldset>
           <legend>
